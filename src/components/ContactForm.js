1b05f9be3c13323afe98c26a64b776a188c5f407
Scoutan <72166266+Scoutan@users.noreply.github.com>
2021-06-15 14:53:45 -0400

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,10 @@ import {
     makeStyles,
     Button, 
     LinearProgress,
-    Card, CardContent
+    Card, CardContent,
+    Paper,
+    Grid,
+    Typography
 } from '@material-ui/core'
 import { Email } from '@material-ui/icons'
 import { Formik, useField, Form } from 'formik'
@@ -16,7 +19,35 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: 'inherit',
         color: '#C5C6C7',
         textAlign: 'center'
+    },
+    form: {
+        backgroundColor: '#FFF',
+        border: '1px solid white',
+        width: '500px',
+        padding: '10px 15px',
+        margin: '0 auto',
+    },
+    input: {
+        width: '300px',
+        padding: '5px 10px',
+        margin: '0 0 0 5px',
+        border: 'none',
+        borderBottom: '1px solid black'
+    },
+    textArea: {
+        resize: 'none',
+        width: '400px',
+        height: '300px',
+        padding: '12px 20px',
+    },
+    error: {
+        color: 'red',
+        padding: '0 0 0 5px'
+    },
+    submit: {
+        width: '100%'
     }
+
 }));
 
 export default function ContactForm(props) {
@@ -26,130 +57,123 @@ export default function ContactForm(props) {
         const [field, meta] = useField(props);
         return (
             <>
-                <label htmlFor={props.id || props.name}>{label}</label>
-                <input className="text-input" {...field} {...props} />
-                {meta.touched && meta.error ? (
-                    <div className="error">{meta.error}</div>
-                ) : null}
+                <Grid item >
+                    <label htmlFor={props.id || props.name}>
+                        <Typography variant="h6" align="left">
+                            {label}
+                            {meta.touched && meta.error ? (
+                                <span className={classes.error}>
+                                    {meta.error}
+                                </span>
+                            ) : null}
+                        </Typography>
+                    </label>
+                    <input className={classes.input} {...field} {...props} />                    
+                </Grid>
             </>
         );        
     }
 
-    const MyCheckbox = ({ children, ...props }) => {
-        const [field, meta] = useField({ ...props, type: 'checkbox' });
-        return (
-            <div>
-                <label className="checkbox-input">
-                    <input type="checkbox" {...field} {...props} />
-                    {children}
-                </label>
-                {meta.touched && meta.error ? (
-                    <div className="error">{meta.error}</div>
-                ) : null}                
-            </div>
-        );
-    };
-
-    const MySelect = ({ label, ...props }) => {
+    const MyTextArea = ({label, ...props}) => {
         const [field, meta] = useField(props);
         return (
-            <div>
-                <label htmlFor={props.id || props.name}>{label}</label>
-                <select {...field} {...props} />
-                {meta.touched && meta.error ? (
-                    <div className="error">{meta.error}</div>
-                ) : null}
-            </div>
+            <>
+                <Grid item>
+                    <label htmlFor={props.id || props.name}>
+                        <Typography variant="h6" align="left">
+                            {label}
+                            {meta.touched && meta.error ? (
+                                <span className={classes.error}>
+                                    {meta.error}
+                                </span>
+                            ) : null}
+                        </Typography>
+                    </label>
+                    <textarea className={classes.textArea} {...field} {...props} />                    
+                </Grid>
+            </>
         );
-    };
+      };
 
     return (
         <Card className={classes.card} id={props.title} elevation={0}>
-            <HeaderBar icon={<Email />} title={props.title} />      
-            <CardContent>            
+            <HeaderBar icon={<Email fontSize="large" />} title={props.title} />      
+            <CardContent>                
                 <Formik
                     initialValues={{
                         firstName: '',
                         lastName: '',
                         email: '',
-                        acceptedTerms: false,
-                        jobType: ''
+                        message: ''
                     }}
                     validationSchema={Yup.object({
                         firstName: Yup.string()
                             .max(15, 'Must be 15 characters or less')
-                            .required('Required'),
+                            .required('(Required)'),
                         lastName: Yup.string()
                             .max(20, 'Must be 20 characters or less')
-                            .required('Required'),
+                            .required('(Required)'),
                         email: Yup.string()
                             .email('Invalid email address')
-                            .required('Required'),
-                        acceptedTerms: Yup.boolean()
-                            .required('Required')
-                            .oneOf([true], 'You must accept the terms and conditions.'),
-                        jobType: Yup.string()
-                            .oneOf(
-                                ['designer', 'development', 'product', 'other'],
-                                'Invalid Job Type'
-                            )
-                            .required('Required')                    
+                            .required('(Required)'),
+                        message: Yup.string()
+                            .min(1, 'Please enter your message')
+                            .required('(Required)'),              
                     })}
                     onSubmit={(values, { setSubmitting }) => {
                         setTimeout(() => {
                             alert(JSON.stringify(values, null, 2));
                             setSubmitting(false)
-                        }, 400);                
+                        }, 1000);                
                     }}
                 >
-                    {({ submitForm, isSubmitting }) => (                
-                    <Form>
-                        <MyTextInput
-                            component={<TextField />}
-                            label="First Name"
-                            name="firstName" 
-                            type="text"
-                            placeholder="Jane"
-                        />
-                        <br />
-                        <MyTextInput
-                            component={<TextField />}
-                            label="Last Name"
-                            name="lastName" 
-                            type="text"
-                            placeholder="Doe"
-                        />
-                        <br />
-                        <MyTextInput
-                            component={<TextField />}
-                            label="Email Address"
-                            name="email" 
-                            type="email"
-                            placeholder="jane@formik.com"
-                        />
-                        <br />
-                        <MySelect label="Job Type" name="jobType">
-                            <option value="">Select a job type</option>
-                            <option value="designer">Designer</option>
-                            <option value="development">Developer</option>
-                            <option value="product">Product Manager</option>
-                            <option value="other">Other</option>
-                        </MySelect>
-                        <br />
-                        <MyCheckbox name="acceptedTerms">
-                            I accept the terms and conditions
-                        </MyCheckbox>                    
-                        {isSubmitting && <LinearProgress />}
-                        <br />
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            disabled={isSubmitting}
-                            onClick={submitForm}
-                        >
-                            Submit
-                        </Button>
-                    </Form>    
+                    {({ submitForm, isSubmitting }) => (                        
+                        <Form>
+                            <Paper className={classes.form} elevation={3}>
+                            <Grid container spacing={3} direction="column" justify="center" alignItems="flex-start" alignContent="center"> 
+                                <MyTextInput
+                                    component={<TextField />}
+                                    label="First Name"
+                                    name="firstName" 
+                                    type="text"
+                                    placeholder="Jane"
+                                />
+                                <MyTextInput
+                                    component={<TextField />}
+                                    label="Last Name"
+                                    name="lastName" 
+                                    type="text"
+                                    placeholder="Doe"
+                                />
+                                <MyTextInput
+                                    component={<TextField />}
+                                    label="Email Address"
+                                    name="email" 
+                                    type="email"
+                                    placeholder="jane@formik.com"
+                                />
+                                <MyTextArea
+                                    component={<TextField />}
+                                    label="Message"
+                                    name="message"
+                                    rows="6"
+                                    placeholder="Message..."
+                                />
+                                <Grid item className={classes.submit}>
+                                    {isSubmitting && <LinearProgress />}
+                                    <br />
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                        onClick={submitForm}
+                                    >
+                                        Submit
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            </Paper>
+                        </Form>     
                     )}            
                 </Formik>
             </CardContent>
