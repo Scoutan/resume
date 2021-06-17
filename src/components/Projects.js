cf0b7e008dca3ac15e1534d9c5876b058154d70e
Scoutan <72166266+Scoutan@users.noreply.github.com>
2021-06-17 17:47:03 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,95 +3,92 @@ import Image from 'next/image'
 import HeaderBar from '../components/HeaderBar'
 import {
     makeStyles,
-    Card, CardContent, CardActionArea, CardMedia,
+    Card, CardContent,
     Grid,
     Typography,
-    Divider
 } from '@material-ui/core'
 import {
     Apps
 } from '@material-ui/icons'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     card: {
         backgroundColor: 'inherit',
         color: '#C5C6C7',
         textAlign: 'center'
     },
-    // icon: {
-    //     '@media (max-width:450px)': {
-    //         fontSize: '1.4rem'  
-    //     }
-    // },
     imageWrapper: {
         position: 'relative',
-        height: '300px'
+        minHeight: '200px'
     },
-    descriptionWrapper: {
-        '& > h3': {
-            color: '#66FCF1'
+    title: {
+        color: '#66FCF1',
+        '@media (max-width:450px)': {
+            fontSize: '2rem'
         }
     }
-}));
+});
 
 export default function Projects(props) {
     const classes = useStyles();
 
-    return (
-        <Card className={classes.card} id={props.title} elevation={0}>
-            <HeaderBar icon={<Apps className={classes.icon} />} title={props.title} />
-            <CardContent>
-                <Grid 
-                    container
-                    spacing={2}
-                    justify="space-evenly"
-                    alignItems="center"
-                >
+    const ProjectsTemplate = (props) => {
+        switch (props.type) {
+            case 'info':
+                return (
+                    <Grid item xs={5} sm={5}>
+                        <Typography gutterBottom variant="h3" className={classes.title}>
+                            {props.title}
+                        </Typography>
+                        <Typography variant="body1">
+                            {props.desc}
+                        </Typography>
+                    </Grid>
+                )
+            case 'image':
+                return (
                     <Grid item xs={5} sm={5} className={classes.imageWrapper}>                     
-                        <a
-                            href="https://chuko-cra.vercel.app/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
+                        <a href={props.href} target="_blank" rel="noopener noreferrer">
                             <Image 
-                                src="/screenshot-chuko.jpg"
-                                alt="Chuko"
+                                src={props.src}
+                                alt={props.alt}
                                 layout='fill'
                                 objectFit='contain'                                
                             />
                         </a>                        
                     </Grid>
-                    <Grid item xs={5} sm={5} className={classes.descriptionWrapper}>
-                        <Typography gutterBottom variant="h3">
-                            Chuko
-                        </Typography>
-                        <Typography variant="body1">
-                            My personal React project!
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} />     
-                    <Grid item xs={12} sm={5} className={classes.descriptionWrapper}>
-                        <Typography gutterBottom variant="h3" >
-                            Compilation of apps
-                        </Typography>
-                        <Typography variant="body1">
-                            From Stephen Grider's React/Redux course
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={5} className={classes.imageWrapper}>                        
-                        <a
-                            href="https://stephen-grider.vercel.app/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image 
-                                src="/screenshot-SG.jpg"
-                                alt="Compilation of apps"
-                                layout='fill'
-                                objectFit='contain'
-                            />
-                        </a>
-                    </Grid>
+                )
+        }
+    }
+
+    return (
+        <Card className={classes.card} id={props.title} elevation={0}>
+            <HeaderBar icon={<Apps />} title={props.title} />
+            <CardContent>
+                <Grid container spacing={2} justify="space-evenly" alignItems="center">
+                    <ProjectsTemplate
+                        type="image"
+                        href="https://chuko-cra.vercel.app/"
+                        src="/screenshot-chuko.jpg"
+                        alt="Chuko"
+                    />
+                    <ProjectsTemplate
+                        type="info"
+                        title="Chuko"
+                        desc="My personal React project"
+                    />
+                    <Grid item xs={12} />
+                    <ProjectsTemplate
+                        type="info"
+                        title="Compilation of apps"
+                        desc="From Stephen Grider's React/Redux course"
+                    />
+                    <ProjectsTemplate
+                        type="image"
+                        href="https://stephen-grider.vercel.app/"
+                        src="/screenshot-SG.jpg"
+                        alt="Compilation of apps"
+                    />
                 </Grid>
             </CardContent>
         </Card>
