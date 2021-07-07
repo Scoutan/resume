2c34613bca340222aa7f21eb236fbb65d7463221
Scoutan <72166266+Scoutan@users.noreply.github.com>
2021-07-07 15:42:00 -0400

    Create Constants.js

---
+++ src/components/Constants.js
@@ -0,0 +1,51 @@
+export const headers = {
+  intro: 'Introduction',
+  projects: 'Projects',
+  skills: 'Technical Skills',
+  timeline: 'My History',
+  contact: 'Contact Info'
+}
+
+export const summary = `
+Freshly self-taught Front-End React Developer with a proven record in troubleshooting and problem solving.
+Relevant competencies include 2 years of experience in troubleshooting G Suite (Google Workspace) products and a diploma in Computer Science with a focus on web development using PHP and MySQL.
+Naturally analytical, self-motivated, well-organized, able to perform under minimal supervision in accordance with deadlines, and a strong advocate for professional integrity.
+`;
+
+export const volkswagen = [
+    "Selected by the service manager to manage the body shop department and as the backup of the foreman of the garage.",
+    "Receive customers to evaluate their needs and create a repair estimation using Audatex, a web app with an industry standardized format.",
+    "Coordinate with partner body shop garages, based on the type of repair and deadline set by the insurance policies or customer.",
+    "Perform quality control inspections on each vehicle prior its delivery to customer.",
+    "Analyze warranty work orders and file any valid claims to Volkswagen Canada for reimbursement.",
+    "Keep track of new or updated recalls and technical bulletins to ensure that repair procedures are followed by the service technicians.",
+    "When necessary, as the foreman of the garage, coordinate all repair work orders with the service technicians to fulfill the expectations on time."
+];
+
+export const telus = [
+    "Tier 2 tech support for G Suite (e.g. Admin Console, Gmail, Calendar, Drive), with a specialization in data migration and mobile apps.",
+    "Specialized in helping client businesses migrate data (e.g. emails, user directories, shared files) to G Suite and deploy business mobile devices.",
+    "Provde best-in-class customer service via phone calls and emails.",
+    "Clearly communicate technical information to non-technical customers",
+    "Accurately document issues, progress, and solutions after each case interaction.",
+    "Proficient in using Salesforce for customer relationship management and case management",
+    "Collaborate with Google engineers to both document and resolve technical bugs.",
+    "Voted by my colleagues for the “Team Spirit” award in 2017 and 2018.",
+    "Chosen by my team manager for the award of “Rising Star” (i.e. high level performance) in 2017 and 2018."
+  ];
+
+  export const concordiaTA = [
+    "Received a commendation letter from the course instructor, highlighting my attentiveness to details and professional integrity.",
+    "Help students in understanding the course materials",
+    "Evaluate and provide feedbacks on assignments and exams within a deadline.",
+    "Coordinate my tasks with the course instructor and other teaching assistants.",
+    "Maintain a good understanding of the course material through preparatory revisions of the upcoming lessons."
+  ];
+
+  export const entechnevision = [
+    "Build web pages for clients using CSS, PHP, JavaScript, and JQuery (e.g. machomer.com, hardsell.org).",
+    "Build CMS modules to interact with database using PHP and MySQL.",
+    "Ensure compatibility with IE6+, Firefox, and Safari.",
+    "Development tool used: Eclipse IDE."
+  ];
+
