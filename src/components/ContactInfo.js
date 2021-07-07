21331d24e5c40a420502b952eb187bbfc8718566
Scoutan <72166266+Scoutan@users.noreply.github.com>
2021-07-07 15:42:05 -0400

    Update ContactInfo.js

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -13,11 +13,23 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: 'inherit',
         color: '#C5C6C7',
         textAlign: 'center',
-        padding: '0 0 100px 0'
+        padding: '0 0 0px 0'
     },
-    email: {
+    textLink: {
         color: '#66FCF1',
         textDecoration: 'none'
+    },
+    email: {
+        fontSize: '2.1rem',
+        '@media (max-width:560px)': {
+            fontSize: '1.7rem',
+        },
+        '@media (max-width:420px)': {
+            fontSize: '1.5rem',
+        }
+    },
+    footer: {
+        margin: '50px 0 0 0'
     }
 }));
 
@@ -28,11 +40,16 @@ export default function ContactInfo(props) {
         <Card className={classes.card} id={props.title} elevation={0}>
             <HeaderBar icon={<Email />} title={props.title} />      
             <CardContent>
-                <Typography variant="h5" gutterBottom>The best way to reach me is by email!</Typography>
-                <a href='mailto:johnson.chungck@gmail.com' className={classes.email}>
-                    <Typography variant="h4" gutterBottom>johnson.chungck@gmail.com</Typography>
+                <Typography variant="h5" gutterBottom>The best way to reach me is by email:</Typography>
+                <a href='mailto:johnson.chungck@gmail.com' className={classes.textLink}>
+                    <Typography variant="h4" gutterBottom className={classes.email}>johnson.chungck@gmail.com</Typography>
                 </a>
-                <Typography variant="h6">Based in Montreal, QC</Typography>
+                <Typography variant="h6" gutterBottom>Based in Montreal, QC</Typography>
+                <a className={classes.textLink} href='https://github.com/Scoutan/resume' target="_blank" rel="noopener noreferrer">
+                    <Typography className={classes.footer}>
+                        View source code on GitHub
+                    </Typography>
+                </a>
             </CardContent>
         </Card>
     );
