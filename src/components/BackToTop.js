ca570de3b11c0d4d4f97c9219c8a9dabb58b1576
Scoutan <72166266+Scoutan@users.noreply.github.com>
2021-07-07 15:41:35 -0400

    Update BackToTop.js

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -31,7 +31,7 @@ function ScrollTop(props) {
         const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
     
         if (anchor) {
-            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });            
         }
     };
 
